d911f22849aa9b753da6acb3637258cd6c0b4e14
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-09-21 03:05:55 +0530

    initialize recursive parser

---
+++ syntex_analysis/topdownparser/recursiveparser.h
@@ -0,0 +1,14 @@
+//
+// Created by hiren on 21/09/19.
+//
+
+#ifndef COMPILER_ALGORITHMS_RECURSIVEPARSER_H
+#define COMPILER_ALGORITHMS_RECURSIVEPARSER_H
+
+#include "topdowngrammer.h"
+#include "../parser.h"
+
+class RecursiveParse:public Parser{
+
+};
+#endif //COMPILER_ALGORITHMS_RECURSIVEPARSER_H

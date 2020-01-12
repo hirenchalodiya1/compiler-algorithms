bee2279a725d51f50278c7f378f1e1515783becb
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2020-01-13 01:38:38 +0530

    Write test case from DFA

--- tests/automatas/dfa.cpp
+++ tests/automatas/dfa.cpp
@@ -1,9 +1,16 @@
-#include <automatas/dfa.h>
-#include "../../libs/prettyprint/include/print.h"
 #include <iostream>
+#include <fstream>
+#include <prettyprint/print.h>
+#include <automatas/dfa.h>
+#include <automatas/create.h>
+
 using namespace std;
-int main(){
-    DFA<int, int> dfa;
-    printf("worked");
+using namespace prettyprint;
+int main() {
+    ifstream fin;
+    fin.open("../tests/inputs/dfa_in");
+    DFA<string, string> dfa = createDFA(fin);
+    cout << dfa;
+    fin.close();
     return 0;
 }

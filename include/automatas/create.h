72a6a09065dcc51203286d46064208ccc406fde1
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2020-01-13 02:15:51 +0530

    Add NFA create method
    - create nfa from file

--- include/automatas/create.h
+++ include/automatas/create.h
@@ -3,6 +3,7 @@
 #define COMPILER_ALGORITHMS_CREATE_H
 
 #include <automatas/dfa.h>
+#include <automatas/nfa.h>
 #include <sstream>
 #include <iostream>
 
@@ -95,4 +96,92 @@ DFA<std::string, std::string> createDFA(std::istream& is){
     return dfa;
 }
 
+NFA<std::string, std::string> createNFA(std::istream& is){
+    NFA<std::string, std::string> nfa;
+    // set epsilon
+    nfa.setEpsilon("?");
+
+    std::string line, temp;
+    char temp_char;
+    unsigned int i, n;
+    // get all symbols
+    if(&is == &std::cin){
+        is.clear();
+        std::cout << "Enter all symbols separated by space and then hit enter:\n";
+    }
+    std::getline(is, line);
+    line.erase(line.find_last_not_of("\r\n") + 1);
+    i = 0;
+    n = line.size();
+    while(i < n){
+        temp.clear();
+        while (i < n and std::isspace(line[i])){
+            i++;
+        }
+        while (i < n and std::isalnum(line[i])){
+            temp.push_back(line[i]);
+            i++;
+        }
+        if(!temp.empty()){
+            nfa.addAlphabet(temp);
+        }
+    }
+    std::string t;
+    // get all states
+    if(&is == &std::cin){
+        std::cout << "Enter all states separated by space and then hit enter:\n";
+    }
+    temp_char = 't';
+    while (temp_char != '\n'){
+        is >> t;
+        nfa.addState(t);
+        temp_char = is.get();
+    }
+    // get staring state
+    if(&is == &std::cin){
+        std::cout << "Enter starting state and then hit enter: ";
+    }is >> t;
+    nfa.setStartingState(t);
+    // final states
+    if(&is == &std::cin){
+        std::cout << "Enter all final states separated by space and then hit enter:\n";
+    }
+    temp_char = 't';
+    while (temp_char != '\n'){
+        is >> t;
+        nfa.addFinalState(t);
+        temp_char = is.get();
+    }
+
+    // transition table
+    if(&is == &std::cin){
+        std::cout << "Enter all transitions in this format <STATE><SPACE>, <SYMBOL><SPACE> - <STATES SEPARATED BY SPACE AND HIT ENTER>:\n";
+        std::cout << "Press CTRL + D (EOF) to end stream\n";
+    }
+
+    const char arrow = '-';
+    const char delimiter= ',';
+
+    std::string symbol, left, right;
+    while (std::getline(is, line)) {
+        line.erase(line.find_last_not_of("\r\n") + 1);
+        std::stringstream s(line);
+        s >> left;
+        if(!nfa.isValidState(t)){continue;}
+        s >> temp_char;
+        if(std::isspace(temp_char)){s >> temp_char;}
+        if(temp_char == EOF){continue;}
+        if(temp_char != delimiter){continue;}
+        s >> symbol;
+        if(!nfa.isValidAlphabet(symbol)){continue;}
+        s >> temp_char;
+        if(std::isspace(temp_char)){s >> temp_char;}
+        if(temp_char == EOF){continue;}
+        if(temp_char != arrow){continue;}
+        while(s >> right){
+            if(nfa.isValidState(right))nfa.addTransition(left, symbol, right);
+        }
+    }
+    return nfa;
+}
 #endif //COMPILER_ALGORITHMS_CREATE_H
